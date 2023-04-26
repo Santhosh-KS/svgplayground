0e82607d3d6cf55137b0fc23e80043711d4e00d8
SANTMAN <santhoshachar08@gmail.com>
2023-04-26 20:28:07 +0200

    Update svg.swift

--- svg.swift
+++ svg.swift
@@ -142,6 +142,7 @@ struct BezierCurve {
   let p:Point
 }
 
+
 struct Arc {
   let radius:Point
   let x_axis_rotation:Bool
@@ -199,6 +200,56 @@ struct Stroke {
   let cap:CapType
   let joinType:LineJoinType
   let style:LineStyle
+}
+
+// The set of elements that control the output of a filter
+enum FilterPremitives {
+  case spotLight
+  case blend
+  case colorMatrix
+  case componentTransfer
+  case composite
+  case convolveMatrix
+  case diffuseLighting
+  case displacementMap
+  case dropShadow
+  case flood
+  case guassianBlur
+  case image
+  case merge
+  case morphology
+  case offset
+  case specularLighting
+  case tile
+  case turbulance
+  case none
+}
+
+// An element which can have graphics elements and other container elements as child elements.
+enum ContainerElements {
+  case a
+  case clipPath
+  case defs
+  case g
+  case marker
+  case mask
+  case pattern
+  case svg
+  case `switch`
+  case symbol
+}
+
+enum FilterFunctions {
+  case blur
+  case brightness
+  case contrast
+  case dropShadow
+  case grayscale
+  case hueRotate
+  case invert
+  case opacity
+  case sepia
+  case saturate
 }
 
 let s = Node.el("svg",
@@ -359,6 +410,44 @@ let n = svg([version(), width(300), height(200), xmlns()],
              text([x(150), y(125), fontSize(60), textAnchor(.middle), fill("white")], ["SVG"]),
              line([x1(10), y1(110), x2(50), y2(150), stroke("black"), strokeWidth(5)])]
 )
+
+/*
+ <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">*/
+
+// svg([version(), width(300), height(200), xmlns()],)
+ /*
+ <rect x="10" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
+  */
+//rect([x(10), y(10), width(300), height(200),stroke("black"),fill("transparent"),  strokeWidth(5)])
+/*
+ <rect x="60" y="10" rx="10" ry="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
+ */
+//rect([x(60), y(10), rx(10), ry(10), width(30), height(30),stroke("black"), fill("transparent"),  strokeWidth(5)])
+/*
+ <circle cx="25" cy="75" r="20" stroke="red" fill="transparent" stroke-width="5"/>
+ <ellipse cx="75" cy="75" rx="20" ry="5" stroke="red" fill="transparent" stroke-width="5"/>
+ */
+//circle([cx(25), cy(75), r(20),stroke("red"), fill("transparent"), strokeWidth(5)])
+
+/*
+ 
+ <line x1="10" x2="50" y1="110" y2="150" stroke="orange" stroke-width="5"/>
+ */
+//line([x1(10),x2(50),y1(110),y2(150),stroke("orange"),strokeWidth(5)])
+
+/*
+ <polyline points="60 110 65 120 70 115 75 130 80 125 85 140 90 135 95 150 100 145"
+ stroke="orange" fill="transparent" stroke-width="5"/>
+ */
+//polyline(<#T##attrs: [(String, String)]##[(String, String)]#>)
+/*
+ <polygon points="50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180"
+ stroke="green" fill="transparent" stroke-width="5"/>
+ 
+ <path d="M20,230 Q40,205 50,230 T90,230" fill="none" stroke="blue" stroke-width="5"/>
+ </svg>
+ */
+
 
 func viewBox(_ value:ViewBox) -> (String, String) {
   return ("viewBox", "\(value.position.x) \(value.position.y) \(value.span.width) \(value.span.height)")
